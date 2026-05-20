3e8c313db8e6b51960a22af3b08aab4e2b090f55
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-05-20 16:49:36 +0400

    update mobilesamv2.sh

--- MobileSAMv2/mobilesamv2.sh
+++ MobileSAMv2/mobilesamv2.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python3 ./Inference.py  \
+CUDA_VISIBLE_DEVICES=0 python ./Inference.py  \
     --img_path './test_images/' \
     --output_dir './' \
     --encoder_type 'efficientvit_l2'
